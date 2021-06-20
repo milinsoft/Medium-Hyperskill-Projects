9cae17b2ac55254b239c5ea26bd3cd2c960f6175
parseltongist <alex.milinkevich@gmail.com>
2021-06-20 20:40:12 +0200

    4/7 stable vesion. clean code.

--- university admission.py
+++ university admission.py
@@ -19,20 +19,14 @@ def sorting_third_priority(applicants_data):
 
 
 def selection(department_accepted, department):
-    # global biotech, engineering, chemistry, physics, mathematics
-    # global biotech_accepted, engineering_accepted, chemistry_accepted, physics_accepted, mathematics_accepted
 
     if len(department_accepted) < max_students_per_department:
         vacant_spots = int(max_students_per_department) - int(len(department_accepted))  # difference between max number of spots and occupied
         # if the number of vacant spots is greater than applicants, we will set num of available spots equal to num of applicants
         if vacant_spots > len(department):
             vacant_spots = len(department)
-        #for counter in range(vacant_spots):
-        #    department_accepted.append(department[counter])
-        #    del department[counter]
         department_accepted.extend(department[0:vacant_spots])
         del department[0:vacant_spots]
-
     else:
         pass
     return department_accepted
@@ -126,29 +120,28 @@ engineering_accepted = []
 mathematics_accepted = []
 physics_accepted = []
 
-first_round_list = sorting_applicants(applicants)
-split_and_invoke_selection(first_round_list, 3)  # index 3 for the 1st priority
-first_round_list.clear()
 
-
-if all([len(biotech_accepted) == max_students_per_department, len(engineering_accepted) == max_students_per_department, len(chemistry_accepted) == max_students_per_department, len(physics_accepted) == max_students_per_department, len(mathematics_accepted) == max_students_per_department]):
-    print_results()
-    exit()  # potentially useless block as repeating the lines below
-else:
-    pass
-
-
-selection_round = 2  # valid value
+selection_round = 1  # default value
 
 
 while selection_round <= 3:
+    if selection_round == 1:
+        first_round_list = sorting_applicants(applicants)
+        split_and_invoke_selection(first_round_list, 3)  # index 3 for the 1st priority
+        selection_round += 1
+        del first_round_list  # deleting from memory
+        if all([len(biotech_accepted) == max_students_per_department, len(engineering_accepted) == max_students_per_department, len(chemistry_accepted) == max_students_per_department, len(physics_accepted) == max_students_per_department, len(mathematics_accepted) == max_students_per_department]):
+            print_results()
+            exit()  # potentially useless block as repeating the lines below
+        else:
+            pass
 
     if selection_round == 2:
         second_round_list = [*biotech, *engineering, *chemistry, *physics, *mathematics]
         second_round_list = sorting_second_priority(second_round_list)
         split_and_invoke_selection(second_round_list, 4)  # index 4 for the 2nd priority
         selection_round += 1
-        second_round_list.clear()
+        del second_round_list  # deleting from memory
         if all([len(biotech_accepted) == max_students_per_department, len(engineering_accepted) == max_students_per_department, len(chemistry_accepted) == max_students_per_department, len(physics_accepted) == max_students_per_department, len(mathematics_accepted) == max_students_per_department]):
             print_results()
             exit()
@@ -159,9 +152,7 @@ while selection_round <= 3:
         # step #8 obtaining the list for the last tour:
         last_round_list = [*biotech, *engineering, *chemistry, *physics, *mathematics]  # unpacking lists with * asterisk sign
         last_round_list = sorting_third_priority(last_round_list)
-        second_round_list.clear()
         split_and_invoke_selection(last_round_list, 5)  # index 5 for the 3rd (last) priority
+        del last_round_list  # deleting from memory
         print_results()
         exit()
-
-        # print(len(biotech_accepted) + len(engineering_accepted) + len(chemistry_accepted) + len(physics_accepted) + len(mathematics_accepted))
